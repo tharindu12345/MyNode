8e97613861e3372675b6192aa35369ba2da5393e
Tharindu Umayanga <tharinduumayangat@gmail.com>
2024-01-12 23:25:41 +0530

    adding blog2 and adding new files

--- file.js
+++ file.js
@@ -10,7 +10,38 @@ const fs = require("fs");
 
 // console.log("last line");
 
-//writeline
-fs.writeFile("./doc/blog1.txt", "hello world", () => {
-  console.log("file was written");
-});
+// //writeline
+// fs.writeFile("./doc/blog1.txt", "hello world", () => {
+//   console.log("file was written");
+// });
+
+// fs.writeFile("./doc/blog2.txt", "you can code with me", () => {
+//   console.log("file was written again");
+// });
+
+// //derectories
+// if (!fs.existsSync("./assets")) {
+//   fs.mkdir("./assets", (err) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log("folder has created");
+//   });
+// } else {
+//   fs.mkdir("./assets", (err) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log("folder was deleted");
+//   });
+// }
+
+//deletedirectory
+if (fs.existsSync("./doc/deleteme.txt")) {
+  fs.unlink("./doc/deleteme.txt", (err) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log("file deleted");
+  });
+}
