b6ff3a5ef661b6198ec76651dad615db8d81be48
Tharindu Umayanga <tharinduumayangat@gmail.com>
2024-01-15 09:44:35 +0530

    adding blog3,4 txt files & adding stream.js file(

--- kawya.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function kawya() {
-  return (
-    <div>
-      <h1>My name is Kawya Wijethunga</h1>
-    </div>
-  )
-}
---
+++ streams.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+
+const readStream = fs.createReadStream("./doc/blog3.txt");
+const writeStream = fs.createWriteStream("./doc/blog4.txt");
+
+// readStream.on("data", (uma) => {
+//   console.log("----NEW CHUNK----");
+//   console.log(uma.toString());
+//   writeStream.write("\nNew Stream\n\n");
+//   writeStream.write(uma);
+// });
+
+//pipe
+readStream.pipe(writeStream);
