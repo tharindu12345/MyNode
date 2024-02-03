45b75317a678221a39daac34badfbbe9b7b0d12b
Tharindu Umayanga <tharinduumayangat@gmail.com>
2024-02-03 21:42:24 +0530

    modify files

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { get } = require("lodash");
 
 const app = express();
 
@@ -12,3 +13,7 @@ app.get("/about", (req, res) => {
   //   res.send("<p>About Page</p>");
   res.sendFile("/views/about.html", { root: __dirname });
 });
+
+app.get('/about-us', (req, res)=>{
+    res.redirect('/about')
+})
