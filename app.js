3533b4a241b7022da07262c4b595cfbbe20abd32
Tharindu Umayanga <tharinduumayangat@gmail.com>
2024-02-03 21:45:32 +0530

    modify files

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ app.get("/about", (req, res) => {
   res.sendFile("/views/about.html", { root: __dirname });
 });
 
-app.get('/about-us', (req, res)=>{
-    res.redirect('/about')
-})
+app.get("/about-us", (req, res) => {
+  res.redirect("/about");
+});
+
+app.use((req, res) => {
+  res.sendFile("/views/404.html", { root: __dirname });
+});
