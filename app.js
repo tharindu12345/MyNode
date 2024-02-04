dac88e2240547ef4174b69743ba5ee4d66f226bd
Tharindu Umayanga <tharinduumayangat@gmail.com>
2024-02-04 12:37:24 +0530

    modify files

--- app.js
+++ app.js
@@ -8,16 +8,20 @@ app.listen(3000);
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", {title : 'Home'});
 });
 app.get("/about", (req, res) => {
-  res.render("about");
+  res.render("about", {title : 'About'});
+});
+
+app.get("/blogs/create", (req, res) => {
+  res.render("create", {title : 'Create'});
 });
 
 app.get("/about-us", (req, res) => {
-  res.redirect("/about");
+  res.redirect("/about", {title : 'About-Us'});
 });
 
 app.use((req, res) => {
-  res.sendFile("/views/404.html", { root: __dirname });
+  res.sendFile("/views/404.html", {title : 'Error'});
 });
